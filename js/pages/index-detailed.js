c2cf3e60a68f0a8e45e6fd91471c966a2b61d55c
thainguyen <thai.nguyen.1093>
2020-09-06 10:15:37 -0500

    custom property page and add js for property page

--- js/pages/index-detailed.js
+++ js/pages/index-detailed.js
@@ -1,22 +1,22 @@
-
+var api = "http://75c151037c03.ngrok.io/shweaain/public/";
 $( document ).ready(function() {
-  $.get( "rating/total", function( data ) {
+  $.get( api + "rating/total", function( data ) {
     $( "#total-rating" ).html( data );
   });
 
-  $.get( "house/total", function( data ) {
+  $.get( api + "house/total", function( data ) {
     $( "#total-house" ).html( data );
   });
 
-  $.get( "client/total", function( data ) {
+  $.get( api + "client/total", function( data ) {
     $( "#total-client" ).html( data );
   });
 
-  $.get( "transaction/total", function( data ) {
+  $.get( api + "transaction/total", function( data ) {
     $( "#total-transaction" ).html( data );
   });
 
-  $.get( "city", function( data ) {
+  $.get( api + "city", function( data ) {
     $("#city").html("<option value='0'>All</option>")
     if (data) {
       data.forEach(function(city) {
@@ -26,7 +26,7 @@ $( document ).ready(function() {
     }
   });
 
-  $.get("apartment",function (data) {
+  $.get( api + "apartment",function (data) {
     $("#house-type").html("<option value='0'>All</option>")
     if (data) {
       data.forEach(function(houseType) {
@@ -46,15 +46,133 @@ function search() {
   var numberOfBedRoom = $('#number-of-bed-room input[type=radio]:checked').val(); // 1 2 3 4 5
   var numberOfBathRoom = $('#number-of-bath-room input[type=radio]:checked').val(); // 1 2 3 4 5
   var numberOfFloor = $('#number-of-floor').val(); // 1 2 3 4 5
+  var sortBy = $('#sort-by').val(); // name, price
+  var target = $('#target').val(); // for rent, For Sale
 
-  var filterDate = {
+  var priceRange = $('#price-range').val(); // 278400;3500000
+  var priceMin = priceRange.split(";")[0]; // 278400
+  var priceMax = priceRange.split(";")[1]; // 3500000
+
+  var squareFeet = $('#square-feet').val();// 3100;3300
+  var squareFeetMin = squareFeet.split(";")[0];// 3100
+  var squareFeetMax = squareFeet.split(";")[1];// 3300
+
+  var yearBuilt = $('#year-built').val(); //
+  var yearBuiltMin = yearBuilt.split(";")[0];//
+  var yearBuiltMax = yearBuilt.split(";")[1];//
+
+  var airCondition = $("#air-condition").prop("checked"); // boolean
+  var microwave = $("#microwave").prop("checked");
+  var windowCoverings = $("#window-coverings").prop("checked");
+  var lawn = $("#lawn").prop("checked");
+
+  var swimmingPool = $("#swimming-pool").prop("checked");
+  var wifi = $("#wifi").prop("checked");
+  var dryer = $("#dryer").prop("checked");
+  var gym = $("#gym").prop("checked");
+
+  var tvCable = $("#tv-cable").prop("checked");
+  var laundry = $("#laundry").prop("checked");
+  var barbeque = $("#barbeque").prop("checked");
+  var refrigerator = $("#refrigerator").prop("checked");
+
+  var sauna = $("#sauna").prop("checked");
+  var washer = $("#washer").prop("checked");
+  var outdoorShower = $("#outdoor-shower").prop("checked");
+
+  var filterData = {
     city: city,
     houseType: houseType,
     numberOfBedRoom: numberOfBedRoom,
     numberOfBathRoom: numberOfBathRoom,
-    numberOfFloor: numberOfFloor
-  }
-  $.get("house/search", filterDate, function (data) {
+    numberOfFloor: numberOfFloor,
+    sortBy: sortBy,
+    target: target,
+    priceMin: priceMin,
+    priceMax: priceMax,
+    squareFeetMin: squareFeetMin,
+    squareFeetMax: squareFeetMax,
+    yearBuiltMin: yearBuiltMin,
+    yearBuiltMax: yearBuiltMax,
 
+    airCondition:airCondition,
+    microwave: microwave,
+    windowCoverings: windowCoverings,
+    lawn: lawn,
+
+    swimmingPool: swimmingPool,
+    wifi: wifi,
+    dryer: dryer,
+    gym: gym,
+
+    tvCable: tvCable,
+    laundry: laundry,
+    barbeque: barbeque,
+    refrigerator: refrigerator,
+
+    sauna: sauna,
+    washer: washer,
+    outdoorShower: outdoorShower
+  }
+
+  $.get(api + "house/search", filterData, function (data) {
+    if (data) {
+
+      $("#btn-total-result").val("Show " + data.length + " Results");
+
+      data.forEach(function(house) {
+
+        if (!house) {
+          return;
+        }
+
+        var ratingStart = "";
+        for (var i =0; i< house.rating; i++) {
+          ratingStart += '<li><i class="ion-android-star"></i></li>';
+        }
+
+        var htmlResponse = '<div class="cf-xs-6 cf-sm-6 cf-lg-4 col-xs-6 col-sm-6 col-md-4 prop-i-col">\n' +
+            '            <div class="prop-i">\n' +
+            '                <a href="#" class="prop-i-img">\n' +
+            '                    <img src="' + house.imageUrl + '" alt="">\n' +
+            '                </a>\n' +
+            '                <div class="prop-i-top">\n' +
+            '                    <p class="prop-i-loc">' + house.city.name + '</p>\n' +
+            '                </div>\n' +
+            '                <h3 class="prop-i-ttl">\n' +
+            '                    <a href="property-2.html" onclick="setIDToSession(' + house.id + ');">' + house.houseType + ' ' + house.squareFeet + ' Sqrt</a>\n' +
+            '                </h3>\n' +
+            '                <ul class="prop-i-rating">\n' + ratingStart +
+            '                </ul>\n' +
+            '                <dl class="prop-i-info">\n' +
+            '                    <dt>\n' +
+            '                        <span class="prop-i-info-icon"><img src="img/propinfo1.png" alt=""></span>\n' +
+            '                        Square Feet\n' +
+            '                    </dt>\n' +
+            '                    <dd>'+ house.squareFeet + '</dd>\n' +
+            '                    <dt>\n' +
+            '                        <span class="prop-i-info-icon"><img src="img/propinfo2.png" alt=""></span>\n' +
+            '                        Bedrooms\n' +
+            '                    </dt>\n' +
+            '                    <dd>' + house.numberOfBedRoom + '</dd>\n' +
+            '                    <dt>\n' +
+            '                        <span class="prop-i-info-icon"><img src="img/propinfo3.png" alt=""></span>\n' +
+            '                        Guests\n' +
+            '                    </dt>\n' +
+            '                    <dd> ' + house.numberOfGuest + ' </dd>\n' +
+            '                </dl>\n' +
+            '                <div class="prop-i-bottom">\n' +
+            '                    <p class="prop-i-price"><span class="prop-i-price-val">$' + house.price + '</span></p>\n' +
+            '                    <p class="prop-i-type">' + house.target + '</p>\n' +
+            '                </div>\n' +
+            '            </div>\n' +
+            '        </div>';
+        $("#search-result").append(htmlResponse);
+      });
+    }
   });
+}
+
+function setIDToSession(id) {
+  localStorage['houseId'] = id;
 }
---
+++ js/pages/property.js
@@ -0,0 +1,48 @@
+var api = "http://75c151037c03.ngrok.io/shweaain/public/";
+
+$( document ).ready(function() {
+
+  //Updated on May 29, 2017 at 3:42 pm
+  var monthShortNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+  var currentDate = new Date();
+  $("#update-time").text("Updated on " + monthShortNames[currentDate.getMonth()] + " " + currentDate.getDate() + ", " + currentDate.getFullYear() + " at " + ((currentDate.getHours() % 12) || (12)) + ":" + currentDate.getMinutes() + " " + (currentDate.getHours() >= 12 ? 'PM' : 'AM')
+  );
+
+  var id = getIDToSessionAndRemove();
+  if (!id){
+    id = 0;
+  }
+  $.get( api + "house/" + id, function( house ) {
+    if (!house) {
+
+      $('#image').prop("src", house.imageUrl);
+      $('#city').text(house.city.name);
+      $('#house-type-feet').text("Comfortable " + house.houseType + ' ' + house.squareFeet + ' Sqrt'); // Comfortable Apartments 15 Sqrt
+
+      var ratingStart = "";
+      for (var i =0; i< house.rating; i++) {
+        ratingStart += '<li><i class="ion-android-star"></i></li>';
+      }
+      $("#rating-star").html(ratingStart);
+
+      $("#status").text(house.status); // rented, sold, for rent, for sale
+      $("#square-feet").text(house.squareFeet); // 1500
+      $("#number-of-bed").text(house.numberOfBedRoom); // 1
+      $("#price").text("$" + house.price); // 1500
+
+      $("#house-type").text(house.houseType); //Apartment
+      $("#number-of-bath").text(house.numberOfBathRoom);
+
+      $("#price-final").text("$" + house.price-final); //$123
+
+    }
+  });
+});
+
+function getIDToSessionAndRemove() {
+  var id = localStorage['houseId'];
+  localStorage.removeItem("houseId");
+  return id;
+}
